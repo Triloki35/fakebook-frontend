6b2409214415b1f0729225ee321515d951a06c6f
Triloki Nath <trilokinath35@gmail.com>
2024-02-15 13:32:36 +0530

    modified getNews function

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -7,7 +7,7 @@ import {
   Skeleton,
   TextField,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CountrySelect from "./CountrySelect";
 import Categories from "./Categories";
 import "./news.css";
@@ -15,36 +15,46 @@ import { ArrowBack } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const News = ({setNews}) => {
+const News = ({ setNews }) => {
   const [query, setQuery] = useState("");
-  const [category, setCategory] = useState("");
   const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(false);
   const [articles, setArticles] = useState(null);
 
-  const KEY = process.env.REACT_APP_NEWS_KEY;
+  const API = process.env.REACT_APP_API;
 
   const getNews = async () => {
     setLoading(true);
     try {
-      const newsApiParams = {
-        country: country.toLowerCase(),
-        category: category.toLowerCase(),
-        apiKey: KEY,
-        q: query,
+      const params = {
+        query: query,
+        country: country,
       };
+      const res = await axios.get(
+        `${API}search/search-news`,
+        {
+          params: params
+        }
+      );
 
-      const res = await axios.get("https://newsapi.org/v2/top-headlines", {
-        params: newsApiParams,
-      });
-
-      setArticles(res.data.articles);
+      // const res = await axios.get(
+      //   `http://localhost:8000/api/search/search-news`,
+      //   {
+      //     params: params,
+      //   }
+      // );
+      // console.log(res.data);
+      setArticles(res.data);
     } catch (error) {
       console.log(error);
     }
     setLoading(false);
   };
 
+  useEffect(() => {
+    getNews();
+  }, [])
+  
 
   const generateSkeletons = (count) => {
     const skeletons = [];
@@ -52,25 +62,22 @@ const News = ({setNews}) => {
     for (let i = 0; i < count; i++) {
       skeletons.push(
         <ListItem>
-          <Skeleton variant="rounded" style={{width:"100%"}} height={150} />
+          <Skeleton variant="rounded" style={{ width: "100%" }} height={150} />
         </ListItem>
       );
     }
 
     return skeletons;
   };
-  // console.log(news[0]);
-
-  // console.log(category);
-  // console.log(country);
-  // console.log(query);
-  // console.log(KEY);
 
   return (
     <Grid>
       <div className="news-search">
         <div className="nw-top">
-          <ArrowBack onClick={()=>setNews(false)} style={{marginRight:"10px"}}/>
+          <ArrowBack
+            onClick={() => setNews(false)}
+            style={{ marginRight: "10px" }}
+          />
           <TextField
             id="standard-basic"
             label="keywords"
@@ -89,35 +96,31 @@ const News = ({setNews}) => {
         </div>
         <div className="nw-bottom">
           <CountrySelect setCountry={setCountry} />
-          <Categories setCategory={setCategory} />
         </div>
       </div>
 
       <List>
         {articles &&
-          articles.map((a,index) => (
+          articles.map((a, index) => (
             <ListItem key={index} className="news-list-item" divider={true}>
               <div className="nw-left">
                 <Avatar
                   variant="rounded"
                   alt="News"
-                  src={a.urlToImage}
+                  src={a?.thumbnail || a?.highlight?.thumbnail || a?.stories?.[0]?.thumbnail}
                   sx={{ height: 100, width: 100 }}
                 />
               </div>
               <div className="nw-right">
-                <span>{a.title}</span>
-                <small>
-                  {a.description}
-                </small>
-                <Link className="news-link" to={a.url}>
+                <span>{a.title || a?.highlight?.title || a?.stories?.[0]?.title}</span>
+                <Link className="news-link" to={a.link || a?.highlight?.link || a?.stories?.[0]?.link}>
                   read full article
                 </Link>
               </div>
             </ListItem>
           ))}
 
-          {loading && generateSkeletons(5)}
+        {loading && generateSkeletons(5)}
       </List>
     </Grid>
   );
